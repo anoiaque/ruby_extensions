e64c9e90b9866dd0417e84ff9f4e3bf7eb21f3e4
anoiaque <anoiaque@gmail.com>
2012-06-23 12:56:58 +0200

    new methods on Array

--- lib/ruby_extend.rb
+++ lib/ruby_extend.rb
@@ -1,23 +1,57 @@
 class Hash
- 
+   
   # Return array from hash recursively sorted
   def to_sorted_array block=nil
-
     sort(&block).inject([]) do |array, (key, value)|
-      array << [key, value.is_a?(Hash) ? value.to_sorted_array : value]
+      value = value.is_a?(Hash) ? value.to_sorted_array : value
+      array << [key, value]
     end
   end
   
 end
 
 class Array
-  
+  require 'set'
   # Recursively convert array to hash 
   def to_hash 
     inject({}) do |hash, (key, value)|
       value = value.to_hash if value.kind_of?(Array)
       hash.merge!({key => value})
     end
+  end
+  
+  def move(from, to)
+    insert(to, delete_at(from))
+    self
+  end
+
+  def included_in? array
+    array.to_set.superset?(self.to_set)
+  end
+
+  def duplicates 
+    uniq.map { |element| diff = (self.size - (self-[element]).size); (diff > 1) ? [element, diff] : nil}.compact
+  end
+
+  def map_with_index
+    result = []
+    each_with_index do |element, index|
+      result << yield(element, index)
+    end
+    result
+  end
+
+  def average
+    if self.respond_to?(:sum)
+      sum.to_f / self.count
+    else
+      self.inject(:+).to_f / self.count
+    end
+  end
+
+  def longest
+    max = group_by(&:size).max
+    max.last.pop if max
   end
   
 end
--- test/test_ruby_extensions.rb
+++ test/test_ruby_extensions.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require File.join(File.dirname(__FILE__),'../lib/ruby_extensions')
+require File.join(File.dirname(__FILE__),'../lib/ruby_extend')
 
 
 class TestRubyExtensions < Test::Unit::TestCase
@@ -25,6 +25,35 @@ class TestRubyExtensions < Test::Unit::TestCase
     expected = {"user"=>  {"name"=>"toto", "adress"=>{"street"=>"rue de Quimper", "town"=>"Paris"}, "age"=>12}}
     assert_equal expected, array.to_hash
   end
-
+  
+  def test_array_included_in
+    assert [1, 3, 766, 9].included_in?([1, 3, 766, 9])
+    assert [1, 3, 766, 9].included_in?([1, 3, 766, 9,10])
+    assert ![1, 3, 4, 9].included_in?([1, 3, 766, 9])
+  end
+  
+  def test_move_an_element_of_an_array
+    a = [1, 2, 3, 4]
+    assert_equal [2, 3, 4, 1], a.move(0, 3)
+  end
+  
+  def test_duplicates
+    assert_equal [[2, 2], [3, 3]], [1, 2, 4, 2, 3, 6, 3, 3].duplicates
+    assert_equal [], [1, 2, 3, 4].duplicates
+  end
+  
+  def test_map_with_index
+    [1, 2, 3, 4].map_with_index do |n, index|
+      assert_equal n, index + 1
+    end
+  end
+  
+  def test_average
+    assert_equal 10, [0, 20].average
+  end
+  
+  def test_longest
+    assert_equal "hello", ["hello", "how", "are", "you"].longest
+  end
   
 end
